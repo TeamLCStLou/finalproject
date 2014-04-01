b4146adf9ec115af121c18106a1bc63e3285aa22
stephens31 <stephens31@users.noreply.github.com>
2014-03-31 21:02:16 -0500

    Create setpermissions.sh
    updates permissions

---
+++ setpermissions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+chmod a+x ~
+chmod a+x ~/vhosts
+chmod a+x ~/vhosts/finalproject
+chmod a+x ~/vhosts/finalproject/public
+
+cd ~/vhosts/finalproject/public
+
+# any php file
+#chmod 600 
+
+# any non-php file
+#chmod 644 
+
+# any directory
+#chmod 711 directory
+
+cd ~/vhosts/finalproject
