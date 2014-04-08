26bced80f69b7536ebea5affa7b122b3e00764cb
stephens31 <stephensmith378+CS50@gmail.com>
2014-04-08 00:57:55 -0400

    project setup

--- setpermissions.sh
+++ setpermissions.sh
@@ -5,9 +5,14 @@ chmod a+x $HOME/vhosts
 chmod a+x $HOME/vhosts/finalproject
 chmod a+x $HOME/vhosts/finalproject/public
 
+echo "*** Scripts: chmod 700"
+echo "$HOME/vhosts/finalproject/*.sh"us
+chmod 700 $HOME/vhosts/finalproject/*.sh
+
+
 # any php file
 #chmod 600 
-echo "php files: chmod 600:"
+echo "*** PHP files: chmod 600:"
 cat $HOME/vhosts/finalproject/php-file-list | while read line
 do
 echo $line
@@ -17,21 +22,22 @@ done
 
 # any non-php file
 #chmod 644 
-echo "non-php files: chmod 644:"
+echo "*** non-PHP files: chmod 644:"
 cat $HOME/vhosts/finalproject/notphp-file-list | while read line
 do
+echo $line
 chmod 644 $HOME/$line
 done
 
 
 # any directory
-#chmod 711 directocat
-echo "Directories: chmod 711:"
+#chmod 711 directory
+echo "*** Directories: chmod 711:"
 cat $HOME/vhosts/finalproject/dir-list | while read line
 do
+echo $line
 chmod 711 $HOME/$line
 done
 
 
 
-cd ~/vhosts/finalproject
