0127269a849cc5d74a2028912083981a2ef7c023
Ragnarok77 <Erocdotusa@gmail.com>
2014-04-09 20:33:54 -0400

    check

---
+++ public/mylcregister.php
@@ -0,0 +1,80 @@
+<h1><u>My LaunchCode!</u></h1>
+<P>
+<P>
+<form action="mylcregister.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="username" placeholder="Username"  type="text" align="center"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="password" placeholder="Password" type="password" align="center"/>
+        <div class="form-group">
+        <input class="form-control" name="confirmation" placeholder="Confirm Password" type="password" align="center"/>
+        </div>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="m-btn">Log In</button>
+        </div>
+       
+    </fieldset>
+</form>
+<div>
+    or <a href="mylclogin.php">log in</a>
+</div>
+
+
+
+
+
+<?php
+
+// TO DO:  config file?  require("../includes/config.php");
+
+require("../includes/MyLCconfig.php");
+
+// if user submits the form
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $uname = ($_POST["username"]);
+    $upassword = ($_POST["password"]);
+    $ulength = strlen($uname);
+    $upasslength = strlen($upassword);
+    
+    if(empty($_POST["username"] || $_POST["password"]))
+    {
+        apologize("Username or password missing! Please enter all fields.");
+    }
+    else if($ulength < 3 || $ulength > 15)
+    {
+        apologize("Your Username needs to be between 3 and 15 characters. Try again!");
+    }
+    else if($upasslength < 5 || $upasslength > 15)
+    {
+        apologize("Your Password needs to be between 5 and 15 characters. Try again!");
+    }
+    else if($_POST["password"] != $_POST["confirmation"])
+    {
+        apologize("Passwords do not match. Try again!");
+    }
+    
+    
+    $result = query("INSERT INTO users (username, password) VALUES (?, ?)", $_POST["username"], crypt($_POST["password"])); 
+        if($result === false)
+        {
+            apologize("Failed to register user. Desired username already exists!");
+        }
+        
+        else
+        {
+            $rows = query("SELECT LAST_INSERT_ID() AS id");
+            $id = $rows[0]["id"];
+            $row = $rows[0];
+            $_SESSION["id"] = $row["id"];
+            redirect("/");
+        }
+    }
+  
+
+?>
---
+++ public/profileEditForm.php
@@ -0,0 +1,76 @@
+<?php
+
+require("../includes/MyLCconfig.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $PhoneNum = ($_POST["Phone number"]);
+    $Email = ($_POST["E-mail address"]);
+    $Location = ($_POST["Location"]);
+    $Experience = ($_POST["Experience"]);
+    $Availability = ($_POST["Availability[]"]);
+    $Lecture = ($_POST["Lecture Location"]);
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Edit Profile</title>
+    </head>
+    <body>
+    <h1>Edit Profile</h1>
+    <p>
+    <p>
+		<div class="content">
+		<form method="post">
+		 <h3>Contact info:</h3>
+		 <p>
+		 <h3>Phone Number:</h3>
+		 <p>
+		 
+		 <input name="Phone number" type="text" placeholder="(XXX)-XXX-XXXX">
+		 <p>
+		 <h3>E-mail:</h3>
+		 <p>
+		 <input name="E-mail address" type="text" placeholder="Name@name.com">
+		 <p>
+		 
+		 <h3>Choose your preferred location:</h3>
+			<select name="Location">
+			<option value="Downtown">Downtown</option>
+            <option value="Jefferson County">Jefferson County</option>
+            <option value="Metro East">Metro East</option>
+            <option value="Midtown">Midtown</option>
+            <option value="North County">North County</option>
+            <option value="South County">South County</option>
+            <option value="St. Charles">St. Charles</option>
+            <option value="West County">West County</option>
+            </select> 
+		 <p>
+		 <h3>Choose your programming experience:</h3>
+		 <select name="Experience">  
+		 <option value="None">None</option>
+		 <option value="Novice">Novice</option>
+		 <option value="Intermediate">Intermediate</option>
+		 <option value="Advanced">Advanced</option>
+		 </select>
+		 <p>
+		 <h3>Choose your availability:</h3>
+		 <input type="checkbox" name="Availability[]" id="Availability" value="Morning"></input> 
+		 <input type="checkbox" name="Availability[]" id="Availability" value="Afternoon"></input> 
+		 <input type="checkbox" name="Availability[]" id="Availability" value="Evening"></input>
+		 <input type="checkbox" name="Availability[]" id="Availability" value="Weekday"></input>
+		 <input type="checkbox" name="Availability[]" id="Availability" value="Weekend"></input>
+		 <p>
+		 <h3>Choose your lecture location preference:</h3>
+		 <input type="radio" name="Lecture Location" value="Soldier's Memorial">Soldier's Memorial<br>
+		 <input type="radio" name="Lecture Location" value="Central Library">Central Library<br>
+		 <p>
+		 <p>
+		 <button type="submit" class="m-btn">Save Changes</button>
+		 
+		 </form>
+		</div>
+	</body>
+</html>  
+		 
