be977a97bf137243284f722a08d02e3082968e15
NickyJack <ernie98@gmail.com>
2014-04-09 21:24:02 -0400

    homepage push

---
+++ public/mylchomepage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+<link href= "css/mylcstyles.css" rel= "stylesheet"/>
+
+<title>MyLaunchCode.com Homepage</title>
+
+</head>
+
+<body>
+
+<h1> HELLO AND WELCOME TO MYLCHOMEPAGE! </h1>
+
+</body>
+
+</html>
