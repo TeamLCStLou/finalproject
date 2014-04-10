32cab970c3580021fdd9f0ef37a47dcbf9f16f92
NickyJack <ernie98@gmail.com>
2014-04-09 21:29:31 -0400

    login

---
+++ public/mylclogin.php
@@ -0,0 +1,41 @@
+<?php
+
+{
+    header('Refresh: 1, mylchomepage.php/');
+}
+
+//MySQL Database Connect
+ /*include 'datalogin.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if (empty($_POST["username"]))
+        {
+            echo "You must enter a valid username.";
+            header('Refresh: 2, mylclogin_form.php/');
+        }
+        
+        if (empty($_POST["password"]))
+        {
+            echo "You must enter a valid password.";
+            header('Refresh: 2, mylclogin_form.php/');
+        }
+        
+        
+        
+        $user = mysqli_query("localhost")
+        
+        ("SELECT * FROM user WHERE username = ?", $_POST["username"]);
+        
+        if (count($user) == 1)
+        {
+            echo "WOOT!";
+        }
+    }
+    
+    else
+    {
+        header('Location: http://pset7/mylclogin_form.php/');
+    }
+    */
+?> 
