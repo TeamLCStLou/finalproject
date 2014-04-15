2453e339d4ec2029806eccf044521bd375ece19b
stephens31 <stephensmith378@yahoo.com>
2014-04-14 22:03:11 -0400

    links

---
+++ public/links.php
@@ -0,0 +1,28 @@
+<style>
+table.links, td,th
+{   
+    border-collapse:collapse;
+    border:1px solid green;
+    text-align:center;
+}    
+
+table.links
+{
+    width:100%;
+}
+    
+</style>
+
+<div>
+    <table class="links">
+        <tr>
+            <th>Quick Link </th>
+        </tr>
+        <tr>
+            <td>link 1</td>
+        </tr>
+        <tr>
+            <td>link 2</td>
+        </tr>
+    </table>
+</div>
