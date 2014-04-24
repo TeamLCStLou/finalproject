0b99e4718ea19cb353e6f0ab3f9b2522a03874ca
stephens31 <stephensmith378@yahoo.com>
2014-04-24 00:05:26 -0400

    link updates

--- templates/links.php
+++ templates/links.php
@@ -1,5 +1,29 @@
-<?php
-    query = 
-    
+<style>
+table.links, td,th
+{   
+    border-collapse:collapse;
+    border:1px solid green;
+    text-align:center;
+}    
 
-?>
+table.links
+{
+    width:100%;
+}
+    
+</style>
+
+<div id="container" style="width:500px">
+    <table class="links">
+        <tr>
+            <th>Quick Links</th>
+        </tr>
+        <?php foreach ($result as $linkitem): ?>
+
+            <tr>
+                <td><a href="<?= $linkitem["link_href"] ?>" </a><?= $linkitem["link_text"]?></td>
+            </tr>
+
+        <?php endforeach ?>
+    </table>
+</div>
