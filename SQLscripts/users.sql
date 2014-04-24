f0d254ca9c2e93393aba8fd6b55ee7c294623b52
stephens31 <stephensmith378@yahoo.com>
2014-04-23 21:20:52 -0400

    sql table

--- SQLscripts/users.sql
+++ SQLscripts/users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 14, 2014 at 07:27 PM
+-- Generation Time: Apr 23, 2014 at 09:15 PM
 -- Server version: 5.5.32-MariaDB
 -- PHP Version: 5.5.3
 
@@ -36,26 +36,27 @@ CREATE TABLE IF NOT EXISTS `users` (
   `LName` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `Email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `Phone` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `Lecture_Loc` int(5) DEFAULT NULL,
-  `City_Loc` int(5) DEFAULT NULL,
-  `Group_ID` int(5) DEFAULT NULL,
-  `ProgExp_ID` int(5) DEFAULT NULL,
-  `Available` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Lecture_Loc` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `City_Loc` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Group_Name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ProgExp` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_on` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `usertype`, `FName`, `LName`, `Email`, `Phone`, `Lecture_Loc`, `City_Loc`, `Group_ID`, `ProgExp_ID`, `Available`, `created_on`) VALUES
-(1, 'ssmith', '12345', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-03-29 05:12:38.000000'),
-(2, 'Erik', '12345', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-03-29 05:34:10.000000'),
-(3, 'Erin', '12345', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-03-29 05:34:10.000000'),
-(4, 'Steve', '12345', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-03-29 05:34:27.000000');
+INSERT INTO `users` (`id`, `username`, `password`, `usertype`, `FName`, `LName`, `Email`, `Phone`, `Lecture_Loc`, `City_Loc`, `Group_Name`, `ProgExp`, `created_on`) VALUES
+(1, 'ssmith', '12345', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-03-29 05:12:38.000000'),
+(2, 'Erik', '12345', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-03-29 05:34:10.000000'),
+(3, 'Erin', '12345', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-03-29 05:34:10.000000'),
+(4, 'Steve', '12345', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-03-29 05:34:27.000000'),
+(5, 'steve', '$1$FYeiyNQW$Kg39We5X1Mrv6li6S88lk/', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-04-15 00:46:45.346603'),
+(7, 'steve_bad', '$1$GeptmpJy$hA/.T8jISD3rbAUVa3Mkb0', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-04-15 03:40:56.666112');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
