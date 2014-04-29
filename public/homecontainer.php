b366803c43ccc664f1e70b8c05eb4c95f247ef15
stephens31 <stephensmith378+cs50@gmail.com>
2014-04-29 00:23:30 -0400

    portal page

--- public/homecontainer.php
+++ public/homecontainer.php
@@ -1,20 +1,51 @@
-<div id="container" style="width:90%;background-color:#FFA500;">
-    <div id="header" style="width:100%;background-color:#EEEEEE;">
-        HEADER
-        
-        MORE HEADERS
+<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
+<style>
+
+#header
+{
+    width:100%;
+    height:75px;
+    background-image: url("../images/bckgnd.png");
+    background-repeat: repeat-x;
+    border-width:1px;
+    border-style:solid;
+}
+
+#footer
+{
+    width:100%;
+    height:75px;
+    background-image: url("../images/bckgnd.png");
+    background-repeat: repeat-x;
+    border-width:1px;
+        border-style:solid;
+    
+}
+
+b
+{
+    font-family: 'PT Sans Narrow', 'sans-serif';
+}    
+</style>
+
+
+<div id="container" style="width:90%;">
+    <div id="header">
+        <img class="imgcenter" alt="MyLaunchcode.com" src="../images/MyLClogo.png" style="height:auto;width:auto;max-height:98%;"/>        
+
     </div>
     <div id="content" style="width:100%">
-        <div id="left-content" style="height:400px;width:70%;float:left;background-color:#FFA500;"" >
-            <table>
+
+        <div id="left-content" style="height:400px;width:70%;float:left;background-color:red;"" >
+            <table style="background-color:white;width:70%;opacity:0.9;" >
                 <tr>
-                    <td>    
-                <img height="150px" border-style="groove"src="http://static2.businessinsider.com/image/4f3433986bb3f7b67a00003c/a-parasite-found-in-cats-could-be-manipulating-our-brains.jpg"/>
+                    <td style="background-color:white;width:30%;">    
+                <img height="150px" style="border-style:groove; border-width:1px;" src="http://static2.businessinsider.com/image/4f3433986bb3f7b67a00003c/a-parasite-found-in-cats-could-be-manipulating-our-brains.jpg"/>
                     </td>
-                <td>
-                    Name: Stephen Smith<br/>
-                    City Location: South City<br/>
-                    Lecture Location: Central Library<br/>
+                <td style="background-color:white;width:70%;text-align:left;">
+                    <b>Name:</b> Stephen Smith<br/>
+                    <b>City Location:</b> South City<br/>
+                    <b>Lecture Location:</b> Central Library<br/>
                 </td>    
                 </tr>
                 
@@ -22,8 +53,10 @@
             
             </table>
         </div>
-        <div id="right-content" style="height:400px;width:30%;float:right;background-color:#00A500;"">
+
+        <div id="right-content" style="height:400px;width:30%;float:right;background-color:#FFCCCC">
             <?php require("../public/links.php"); ?>
         </div>
-    </div>
+    </div>  
 </div>
+
