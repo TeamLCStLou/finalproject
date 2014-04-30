43d264a554937d397e16956bc0bbc812abf6a488
stephens31 <stephensmith378+cs50@gmail.com>
2014-04-30 19:58:17 -0400

    portal page

--- SQLscripts/siteLinks.sql
+++ SQLscripts/siteLinks.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 23, 2014 at 09:26 PM
+-- Generation Time: Apr 30, 2014 at 07:57 PM
 -- Server version: 5.5.32-MariaDB
 -- PHP Version: 5.5.3
 
@@ -31,18 +31,21 @@ CREATE TABLE IF NOT EXISTS `siteLinks` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `link_text` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'link',
   `link_href` varchar(600) COLLATE utf8_unicode_ci NOT NULL,
+  `order` int(10) NOT NULL,
   `spotlight` tinyint(1) NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `siteLinks`
 --
 
-INSERT INTO `siteLinks` (`id`, `link_text`, `link_href`, `spotlight`, `created_on`) VALUES
-(1, 'Launch Code', 'http://launchcodestl.com/', 0, '2014-04-15 01:08:23'),
-(2, 'CS50X', 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0CCkQFjAA&url=https%3A%2F%2Fwww.edx.org%2Fcourse%2Fharvardx%2Fharvardx-cs50x-introduction-computer-1022&ei=aYZMU-PGGOab2wXx_oHADg&usg=AFQjCNGqFtoNWLmIaQOFGbvRBNa4hoqYEw&sig2=JoqVaLVAg9rbCF4C80zkng&bvm=bv.64764171,d.b2I', 0, '2014-04-15 01:08:23');
+INSERT INTO `siteLinks` (`id`, `link_text`, `link_href`, `order`, `spotlight`, `created_on`) VALUES
+(1, 'Launch Code', 'http://launchcodestl.com/', 2, 0, '2014-04-15 01:08:23'),
+(2, 'CS50X', 'https://courses.edx.org/courses/HarvardX/CS50x/2014_T1/', 1, 0, '2014-04-15 01:08:23'),
+(3, 'Launch Code CS50 Reddit', 'http://www.reddit.com/r/LaunchCodeCS50x/', 3, 0, '2014-04-29 04:54:56'),
+(6, 'LaunchCode Syllabus', 'http://imgur.com/a/zQYZB', 4, 0, '2014-04-29 05:03:19');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- public/homecontainer.php
+++ public/homecontainer.php
@@ -22,11 +22,21 @@
     
 }
 
-#left-content
+* (better) have a <div> that is position: absolute before #main and the same height as #main, then apply the background image and opacity: 0.2; filter: alpha(opacity=20);
+
+#left-content-bkgrd
 {
     background-image: url("http://elitemarketingevents.com/elitewp/wp-content/uploads/2013/08/City-of-St-Louis-skyline.jpg");
     background-repeat: no-repeat;
     background-size:cover;
+    Opacity:0.2;
+}
+
+#left-content
+{
+//    background-image: url("http://elitemarketingevents.com/elitewp/wp-content/uploads/2013/08/City-of-St-Louis-skyline.jpg");
+//    background-repeat: no-repeat;
+//    background-size:cover;
 //    Opacity:0.2;
 }
 b
@@ -36,15 +46,16 @@ b
 </style>
 
 
-<div id="container" style="width:90%;">
+<div id="container" style="width:90%;height="800px;">
     <div id="header">
         <img class="imgcenter" alt="MyLaunchcode.com" src="../images/MyLClogo.png" style="height:auto;width:auto;max-height:98%;"/>        
 
     </div>
     
     <div id="content" style="width:100%">
-        <div id="left-content" style="height:400px;width:75%;float:left;background-color:red;" >
+        <div id="left-content" style="height:600px;width:75%;float:left;background-color:red;" >
             <table style="background-color:red;width:100%;opacity:0.9;" >
+              <img class="imgcenter" alt="MyLaunchcode.com" src="../images/MyLClogo.png" style="height:auto;width:auto;max-height:98%;"/>   
               <tr>
                 <td style="background-color:red;width:30%;">    
                     <img height="150px" style="border-style:groove; border-width:1px;" src="http://static2.businessinsider.com/image/4f3433986bb3f7b67a00003c/a-parasite-found-in-cats-could-be-manipulating-our-brains.jpg"/>
@@ -58,7 +69,7 @@ b
             </table>
         </div>
 
-        <div id="right-content" style="height:400px;width:25%;float:right;background-color:#FFCCCC;">
+        <div id="right-content" style="height:600px;width:25%;float:right;background-color:#FFCCCC;">
             <?php require("../public/links.php"); ?>
         </div>
                
