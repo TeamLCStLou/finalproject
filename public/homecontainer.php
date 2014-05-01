c78d8caf82b09e8453e06fc82dc8906d74405ecd
stephens31 <stephensmith378@yahoo.com>
2014-04-30 20:39:55 -0400

    fixed home

--- public/homecontainer.php
+++ public/homecontainer.php
@@ -22,21 +22,11 @@
     
 }
 
-* (better) have a <div> that is position: absolute before #main and the same height as #main, then apply the background image and opacity: 0.2; filter: alpha(opacity=20);
-
-#left-content-bkgrd
+#left-content
 {
     background-image: url("http://elitemarketingevents.com/elitewp/wp-content/uploads/2013/08/City-of-St-Louis-skyline.jpg");
     background-repeat: no-repeat;
     background-size:cover;
-    Opacity:0.2;
-}
-
-#left-content
-{
-//    background-image: url("http://elitemarketingevents.com/elitewp/wp-content/uploads/2013/08/City-of-St-Louis-skyline.jpg");
-//    background-repeat: no-repeat;
-//    background-size:cover;
 //    Opacity:0.2;
 }
 b
@@ -46,16 +36,15 @@ b
 </style>
 
 
-<div id="container" style="width:90%;height="800px;">
+<div id="container" style="width:90%;">
     <div id="header">
         <img class="imgcenter" alt="MyLaunchcode.com" src="../images/MyLClogo.png" style="height:auto;width:auto;max-height:98%;"/>        
 
     </div>
     
     <div id="content" style="width:100%">
-        <div id="left-content" style="height:600px;width:75%;float:left;background-color:red;" >
+        <div id="left-content" style="height:400px;width:75%;float:left;background-color:red;" >
             <table style="background-color:red;width:100%;opacity:0.9;" >
-              <img class="imgcenter" alt="MyLaunchcode.com" src="../images/MyLClogo.png" style="height:auto;width:auto;max-height:98%;"/>   
               <tr>
                 <td style="background-color:red;width:30%;">    
                     <img height="150px" style="border-style:groove; border-width:1px;" src="http://static2.businessinsider.com/image/4f3433986bb3f7b67a00003c/a-parasite-found-in-cats-could-be-manipulating-our-brains.jpg"/>
@@ -69,7 +58,7 @@ b
             </table>
         </div>
 
-        <div id="right-content" style="height:600px;width:25%;float:right;background-color:#FFCCCC;">
+        <div id="right-content" style="height:400px;width:25%;float:right;background-color:#FFCCCC;">
             <?php require("../public/links.php"); ?>
         </div>
                
